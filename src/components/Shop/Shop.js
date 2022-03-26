3d9bdb98fa538e771724ae8cf8815ec60187dcbc
dev-sibli <siblisadik13@gmail.com>
2022-03-26 21:09:59 +0600

    question answered

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -36,6 +36,39 @@ const Shop = () => {
 					{guns.map((gun) => (
 						<Gun key={gun.id} gun={gun} handleCart={handleCart}></Gun>
 					))}
+					<div className="m-3">
+						<h3>How React works?</h3>
+						<p>
+							React creates a VIRTUAL DOM in memory. Instead of manipulating the
+							browser's DOM directly, React creates a virtual DOM in memory,
+							where it does all the necessary manipulating, before making the
+							changes in the browser DOM. then changes only what needs to be
+							changed
+						</p>
+						<h3>Props Vs State</h3>
+						<h4>Props</h4>
+						<ul>
+							<li>Props are read-only.</li>
+							<li>Props are immutable.</li>
+							<li>
+								Props allow you to pass data from one component to other
+								components as an argument.
+							</li>
+							<li>Props can be accessed by the child component.</li>
+							<li>Props are used to communicate between components.</li>
+						</ul>
+						<h4>State</h4>
+						<ul>
+							<li>State changes can be asynchronous.</li>
+							<li>State is mutable.</li>
+							<li>State holds information about the components.</li>
+							<li>State cannot be accessed by child components.</li>
+							<li>
+								States can be used for rendering dynamic changes with the
+								component.
+							</li>
+						</ul>
+					</div>
 				</div>
 				<div className="col-lg-3 col-sm-12 card pt-3 mt-4">
 					{cart.map((item) => (
